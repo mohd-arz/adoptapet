67012346b1a0cc2db81f49c9b59a29899238e4e9
mohd-arz <mohammedarsh75@gmail.com>
2024-08-10 19:50:27 +0530

    gravity added cloudinary

--- src/app/_components/sellers/pets/cloudinary-img.tsx
+++ src/app/_components/sellers/pets/cloudinary-img.tsx
@@ -2,7 +2,7 @@
 import { CldImage } from 'next-cloudinary';
 
 // By default, the CldImage component applies auto-format and auto-quality to all delivery URLs for optimized delivery.
-export default function CloudImage({src,class_,width,height,alt}:{src:any,class_:string,width:number,height:number,alt:string}) {
+export default function CloudImage({src,class_,width,height,alt}:{src:string,class_:string,width:number,height:number,alt:string}) {
   return (
     <CldImage
       src={src} // Use this sample image or upload your own via the Media Explorer
@@ -10,6 +10,7 @@ export default function CloudImage({src,class_,width,height,alt}:{src:any,class_
       width={width}
       height={height}
       alt={alt}
+      gravity='auto'
       crop={{
         type: 'auto',
         source: true
