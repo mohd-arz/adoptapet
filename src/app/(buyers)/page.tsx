67d0346b6a586697ab87d9d848585e6827390abf
mohd-arz <mohammedarsh75@gmail.com>
2024-09-07 16:49:16 +0530

    build verified

--- src/app/(buyers)/page.tsx
+++ src/app/(buyers)/page.tsx
@@ -1,5 +1,5 @@
+"use client"
 import Link from "next/link";
-import { api } from "~/trpc/server";
 import { Header } from "../_components/home/header";
 import { SwiperContainer } from "../_components/home/swiper";
 import Button from "../_components/home/button";
@@ -52,7 +52,7 @@ export default async function(){
               Ready to find your pet? Let's do it.
               </p>
               <div className="flex gap-4 items-center">
-                <Button type="black" title="How it Works"/>
+                <Button type="black" title="How it Works" onclick={()=>{}}/>
                 <Link className="group text-black transition-all duration-300 ease-in-out" href="#" >
                   <span className="bg-left-bottom bg-gradient-to-r from-black to-black bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"> 
                   More about us
