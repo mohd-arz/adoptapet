20a10829e66810338b459a93aaca0a37716331ce
mohd-arz <mohammedarsh75@gmail.com>
2024-09-07 16:31:03 +0530

    merge conflict fixed

--- src/app/(buyers)/pet/[id]/page.tsx
+++ src/app/(buyers)/pet/[id]/page.tsx
@@ -1,20 +1,32 @@
 "use client";
 
-import { inferRouterOutputs } from "@trpc/server"
-import { ArrowLeftIcon, ChevronDown, ChevronLeft, ChevronUp, Mail } from "lucide-react"
-import { notFound, useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
-import { AppRouter } from "~/server/api/root"
-import { api } from "~/trpc/react"
-import { differenceInMonths } from 'date-fns';
-import Image from "next/image"
-import { List, ListItem, Collapse } from '@mui/material';
-import { Button } from "~/components/ui/button"
-import DetailPageSkeleton from "~/app/_components/home/detail-page-skeleton"
-import { Gloock}from '@next/font/google'
-import DOMPurify from 'dompurify';
-import { CldImage } from "next-cloudinary"
-import { getSecureUrl } from "~/lib/utils"
+import { inferRouterOutputs } from "@trpc/server";
+import {
+  ArrowLeftIcon,
+  ChevronDown,
+  ChevronLeft,
+  ChevronUp,
+  Mail,
+} from "lucide-react";
+import { notFound, useRouter } from "next/navigation";
+import { useEffect, useState, useTransition } from "react";
+import { AppRouter } from "~/server/api/root";
+import { api } from "~/trpc/react";
+import { differenceInMonths } from "date-fns";
+import Image from "next/image";
+import { List, ListItem, Collapse } from "@mui/material";
+import { Button } from "~/components/ui/button";
+import DetailPageSkeleton from "~/app/_components/home/detail-page-skeleton";
+import { Gloock } from "@next/font/google";
+import DOMPurify from "dompurify";
+import { CldImage } from "next-cloudinary";
+import { getSecureUrl } from "~/lib/utils";
+import { useSession } from "next-auth/react";
+import { db } from "~/server/db";
+import { toast } from "~/components/ui/use-toast";
+import { Toaster } from "~/components/ui/toaster";
+import { sendMail } from "~/lib/action";
+import { ToastAction } from "~/components/ui/toast";
 
 const gloock = Gloock({
   weight: ["400"],
@@ -42,7 +54,12 @@ export default function ({ params }: { params: { id: string } }) {
       const now = new Date();
       const monthsAgo = differenceInMonths(now, data.pet.createdAt);
       setDate(Math.abs(monthsAgo));
-      console.log(getSecureUrl(data?.pet?.image_url as string,data.pet?.thumb_url as string));
+      console.log(
+        getSecureUrl(
+          data?.pet?.image_url as string,
+          data.pet?.thumb_url as string,
+        ),
+      );
       console.log(data?.pet);
     }
   }, [isSuccess, data]);
@@ -51,69 +68,128 @@ export default function ({ params }: { params: { id: string } }) {
   if (isLoading) {
     return <DetailPageSkeleton />;
   }
-  
-  if(isSuccess && data)
-  return (
-    <div className="max-w-[1400px] px-[5%] mx-auto grid grid-cols-12">
-    <div className="col-span-10 col-start-2 gap-x-1">
-      <button className="my-6 flex text-dark-cyan items-center group transition-all duration-300 ease-in-out" onClick={() => router.back()}>
-        <ChevronLeft size={16} />
-        <span className="font-semibold text-sm bg-left-bottom bg-gradient-to-r from-black to-black bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] transition-all duration-500 ease-out">
-          Back
-        </span>
-      </button>
-      <div className="border border-black">
-        <h1 className={`text-5xl my-4 ${gloock.className}`}>My Name is {pet?.name} !</h1>
-        <h2 className="my-4">Posted on {date === 0 ? 'this month' : `${date} month${date as number > 1 ? 's' : ''} ago`}</h2>
-        <div className="grid grid-cols-10 gap-x-8">
-          <div className="border border-black w-full flex flex-col gap-4 col-span-6">
-            <div className="relative h-[400px] bg-slate-800">
-            <Image
-                src={imgInd === -1 ? getSecureUrl(pet?.image_url as string, pet?.thumb_url as string) : getSecureUrl(pet?.SubImages[imgInd]?.sub_url as string, pet?.SubImages[imgInd]?.ext as string)}
-                fill // Replaces layout="fill"
-                style={{ objectFit: 'contain' }} // Replaces objectFit="contain"
-                alt={`${pet?.name}'s profile pictures`}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
-              {/* <CldImage 
-                src={imgInd === -1 ? `${pet?.image_url}` : `${pet?.SubImages[imgInd]?.sub_url}`}
-                width={350} 
-                height={350} 
-                style={{objectFit:'contain',aspectRatio:'1/1'}}  
-                quality={'auto'}
-                format='auto'
-                gravity='auto'
-                crop={'fill'} 
-                alt={`${pet?.name}'s profile pictures`}
-              /> */}
+
+  if (isSuccess)
+    return (
+      <div className="mx-auto grid max-w-[1400px] grid-cols-12 px-[5%]">
+        <div className="col-span-10 col-start-2 gap-x-1">
+          <button
+            className="group my-6 flex items-center text-dark-cyan transition-all duration-300 ease-in-out"
+            onClick={() => router.back()}
+          >
+            <ChevronLeft size={16} />
+            <span className="bg-gradient-to-r from-black to-black bg-[length:0%_1px] bg-left-bottom bg-no-repeat text-sm font-semibold transition-all duration-500 ease-out group-hover:bg-[length:100%_1px]">
+              Back
+            </span>
+          </button>
+          <div className="border border-black">
+            <h1 className={`my-4 text-5xl ${gloock.className}`}>
+              My Name is {pet?.name} !
+            </h1>
+            <h2 className="my-4">
+              Posted on{" "}
+              {date === 0
+                ? "this month"
+                : `${date} month${(date as number) > 1 ? "s" : ""} ago`}
+            </h2>
+            <div className="grid grid-cols-10 gap-x-8">
+              <div className="col-span-6 flex w-full flex-col gap-4 border border-black">
+                <div className="relative h-[400px] bg-slate-800">
+                  <Image
+                    src={
+                      imgInd === -1
+                        ? getSecureUrl(
+                            pet?.image_url as string,
+                            pet?.thumb_url as string,
+                          )
+                        : getSecureUrl(
+                            pet?.SubImages[imgInd]?.sub_url as string,
+                            pet?.SubImages[imgInd]?.ext as string,
+                          )
+                    }
+                    fill // Replaces layout="fill"
+                    style={{ objectFit: "contain" }} // Replaces objectFit="contain"
+                    alt={`${pet?.name}'s profile pictures`}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  />
+                </div>
+                <div className="relative grid w-full grid-cols-5 gap-4">
+                  <CldImage
+                    src={`${pet?.image_url}`}
+                    onClick={() => handleClick(-1)}
+                    className={imgInd === -1 ? "opacity-50" : ""}
+                    alt="main image"
+                    width={100}
+                    height={100}
+                    quality={50}
+                    sizes=""
+                    style={{
+                      aspectRatio: "1/1",
+                      objectFit: "cover",
+                    }}
+                  />
+                  {pet?.SubImages &&
+                    pet.SubImages.map((img, ind) => (
+                      <Image
+                        key={ind}
+                        src={`${getSecureUrl(img.sub_url)}`}
+                        onClick={() => handleClick(ind)}
+                        className={imgInd === ind ? "opacity-50" : ""}
+                        alt="sub images"
+                        width={100}
+                        height={100}
+                        style={{
+                          aspectRatio: "1/1",
+                          objectFit: "cover",
+                        }}
+                      />
+                    ))}
+                </div>
+              </div>
+              <SideBox pet={pet as petType} />
             </div>
-            <div className="grid grid-cols-5 gap-4 relative w-full">
-              <CldImage
-                src={`${pet?.image_url}`}
-                onClick={() => handleClick(-1)}
-                className={imgInd === -1 ? 'opacity-50' : ''}
-                alt="main image"
-                width={100}
-                height={100}
-                quality={50}
-                sizes=""
-                style={{
-                  aspectRatio: '1/1',
-                  objectFit: 'cover'
-                }}
-              />
-              {pet?.SubImages && pet.SubImages.map((img, ind) => (
-                <Image
-                  key={ind}
-                  src={`${getSecureUrl(img.sub_url)}`}
-                  onClick={() => handleClick(ind)}
-                  className={imgInd === ind ? 'opacity-50' : ''}
-                  alt="sub images"
-                  width={100}
-                  height={100}
-                  style={{
-                    aspectRatio: '1/1',
-                    objectFit: 'cover'
+            <div className="basic-info max-h-[12.425rem] border border-black">
+              <h1 className="mb-4 text-3xl font-semibold">My basic info</h1>
+
+              {pet?.type === "DOG" && (
+                <div className="mb-2 flex gap-4">
+                  <div className="font-semibold">Breed</div>
+                  <div>{pet?.breed?.name}</div>
+                </div>
+              )}
+
+              {pet?.type === "OTHERS" && (
+                <div className="mb-2 flex gap-4">
+                  <div className="font-semibold">Type</div>
+                  <div>{pet?.other}</div>
+                </div>
+              )}
+              <div className="mb-2 flex gap-4">
+                <div className="font-semibold">Age</div>
+                <div>{pet?.age}</div>
+              </div>
+
+              <div className="mb-2 flex gap-4">
+                <div className="font-semibold">Sex</div>
+                <div>{pet?.sex}</div>
+              </div>
+            </div>
+
+            <div className="info mt-4">
+              <h1 className="text-3xl font-semibold">Why I need a new home</h1>
+              <p>{pet?.why}</p>
+            </div>
+
+            {DOMPurify.sanitize(pet?.story as string).length > 0 && (
+              <div className="whynewhome mt-4">
+                <h1 className="text-3xl font-semibold">My story</h1>
+                <h2 className="text-xl font-semibold">
+                  Here's what the humans have to say about me:
+                </h2>
+                <div
+                  className="mt-4"
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(pet?.story as string),
                   }}
                 ></div>
               </div>
